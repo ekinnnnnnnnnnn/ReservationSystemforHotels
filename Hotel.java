eba5a80fd0b2528e09a6635c0a82f449e21dfd18
ekinc <ekincaglar2004@gmail.com>
2025-12-12 14:35:33 +0300

    hotel class skeleton added.

---
+++ Hotel.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Hotel {
+    private List<Room> rooms;
+    private List<Reservation> reservations;
+    private final String ROOM_FILE = "rooms.csv";
+    private final String RES_FILE = "reservations.csv";
+
+    public Hotel() {
+        rooms = new ArrayList<>();
+        reservations = new ArrayList<>();
+    }
+    private void loadData() {
+    
+    }
+    public void saveData() {
+
+    }
+    public void searchAvailableRooms() {
+
+    }
+    public void bookRoom() {
+
+    }
+    public void cancelReservation() {
+
+    }
+    public void listReservations() {
+
+    }
+    private void initializeDefaultRooms() {
+
+    }
+
+
+
+
+}
